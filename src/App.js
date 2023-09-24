2043c267f60a1b002cc56ec11021edae0453f2e5
PlanetofJay <jae44dv@gmail.com>
2023-09-24 16:57:54 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+// Import each of pages
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import About from './pages/About';
+import Lookbook from './pages/Lookbook';
+import Category from './pages/Category';
+import News from './pages/News';
+import Partnership from './pages/Partnership';
+
+// Import each of components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <Router>
+        <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/shop' element={<Shop />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/lookbook' element={<Lookbook />} />
+            <Route path='/category' element={<Category />} />
+            <Route path='/partnership' element={<Partnership />} />
+            <Route path='/news' element={<News />} />
+          </Routes>
+        <Footer />
+      </Router>
 
-      </header>
+
+
+
+
+
+
+
     </div>
   );
 }
-
-export default App;
---
+++ src/pages/Partnership.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Partnership() {
+  return (
+    <div>Partnership</div>
+  )
+}
+
+export default Partnership
