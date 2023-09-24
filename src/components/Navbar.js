8999dbd26638579507360da4a0bcedb2a5095810
PlanetofJay <jae44dv@gmail.com>
2023-09-24 17:05:44 -0400

    test hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+// import '../styles/Navbar.css';
 
 function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
   return (
-    <div>Navbar</div>
+    <div className='navbar' id={expandNavbar ? 'open' : 'close'}>
+      <div className='toggleBtn'>
+        <button 
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        />
+      </div>
+
+      <div className='links'>
+        <Link to='/'>Home</Link>
+      </div> 
+    </div>
   )
 }
 
